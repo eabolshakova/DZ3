18c9ef9fe8d6ff910a394b9e963fcbc73dad9561
eabolshakova <73272038+eabolshakova@users.noreply.github.com>
2021-12-19 16:05:43 +0300

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,66 @@
+import main
+import math
+
+
+def test1():
+    counts = main.read('test1.txt')
+    assert main.minimum(counts) == min(counts)
+    assert main.maximum(counts) == max(counts)
+    assert main.sum(counts) == sum(counts)
+    assert main.multiplication(counts) == math.prod(counts)
+
+
+def test2():
+    counts = main.read('test2.txt')
+    assert main.minimum(counts) == min(counts)
+    assert main.maximum(counts) == max(counts)
+    assert main.sum(counts) == sum(counts)
+    assert main.multiplication(counts) == math.prod(counts)
+
+
+def test3():
+    counts = main.read('test3.txt')
+    assert main.minimum(counts) == min(counts)
+    assert main.maximum(counts) == max(counts)
+    assert main.sum(counts) == sum(counts)
+    assert main.multiplication(counts) == math.prod(counts)
+
+
+def test4():
+    counts = main.read('test4.txt')
+    assert main.minimum(counts) == min(counts)
+    assert main.maximum(counts) == max(counts)
+    assert main.sum(counts) == sum(counts)
+    assert main.multiplication(counts) == math.prod(counts)
+
+
+def test5():
+    counts = main.read('test5.txt')
+    assert main.minimum(counts) == min(counts)
+    assert main.maximum(counts) == max(counts)
+    assert main.sum(counts) == sum(counts)
+    assert main.multiplication(counts) == math.prod(counts)
+
+
+def test6():
+    counts = main.read('test6.txt')
+    assert main.minimum(counts) == min(counts)
+    assert main.maximum(counts) == max(counts)
+    assert main.sum(counts) == sum(counts)
+    assert main.multiplication(counts) == math.prod(counts)
+
+
+def test7():
+    counts = main.read('test7.txt')
+    assert main.minimum(counts) == min(counts)
+    assert main.maximum(counts) == max(counts)
+    assert main.sum(counts) == sum(counts)
+    assert main.multiplication(counts) == math.prod(counts)
+
+
+def test8():
+    counts = main.read('test8.txt')
+    assert main.minimum(counts) == min(counts)
+    assert main.maximum(counts) == max(counts)
+    assert main.sum(counts) == sum(counts)
+    assert main.multiplication(counts) == math.prod(counts)
